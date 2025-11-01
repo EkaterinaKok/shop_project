9b3274b8a4b8667571fa6066ffe963e90db4f90e
User-PC <2356532@mail.ru>
2025-11-01 19:57:51 +0500

    ДЗ теме "ООП: полиморфизм, интерфейсы."

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,10 +1,15 @@
 package org.skypro.skyshop;
 
+import org.skypro.skyshop.articles.Article;
+import org.skypro.skyshop.articles.SearchEngine;
+import org.skypro.skyshop.articles.Searchable;
 import org.skypro.skyshop.basket.ProductBasket;
 import org.skypro.skyshop.product.DiscountedProduct;
 import org.skypro.skyshop.product.FixPriceProduct;
 import org.skypro.skyshop.product.Product;
 import org.skypro.skyshop.product.SimpleProduct;
+
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
@@ -12,8 +17,8 @@ public class App {
         Product product1 = new SimpleProduct("Огурец", 50);
         Product product2 = new SimpleProduct("Яблоко", 100);
         Product product3 = new SimpleProduct("Лук", 30);
-        Product discountedProduct1 = new DiscountedProduct("Лимон", 60,20);
-        Product discountedProduct2 = new DiscountedProduct("Перец", 150,20);
+        Product discountedProduct1 = new DiscountedProduct("Лимон", 60, 20);
+        Product discountedProduct2 = new DiscountedProduct("Перец", 150, 20);
         Product fixPriceProduct1 = new FixPriceProduct("Шоколад");
         Product fixPriceProduct2 = new FixPriceProduct("Печенье");
 
@@ -79,5 +84,64 @@ public class App {
         //10. Поиск товара по имени в пустой корзине.
         System.out.println("\nПоиск товара по имени в пустой корзине:");
         basket1.productSearch("Лук");
+
+        //ДЗ по разделу ООП: полиморфизм и интерфейсы
+        SearchEngine searchEngine = new SearchEngine(11);
+        searchEngine.add(product1);
+        searchEngine.add(product2);
+        searchEngine.add(product3);
+        searchEngine.add(discountedProduct1);
+        searchEngine.add(discountedProduct2);
+        searchEngine.add(fixPriceProduct1);
+        searchEngine.add(fixPriceProduct2);
+
+        Searchable article1 = new Article("Заголовок 1", "Интересный текст");
+        Searchable article2 = new Article("Заголовок 2", "Важный текст");
+        Searchable article3 = new Article("Заголовок 3", "Короткий текст");
+        Searchable article4 = new Article("Заголовок 4", "Длинный текст");
+
+        searchEngine.add(article1);
+        searchEngine.add(article2);
+        searchEngine.add(article3);
+        searchEngine.add(article4);
+
+        System.out.println("\nВывод нового элеманта статья и ее текст:");
+        System.out.println(article1.getStringRepresentation());
+
+        System.out.println("\nВывод продукта через новый элемент:");
+        System.out.println(product1.getStringRepresentation());
+        System.out.println(discountedProduct1.getStringRepresentation());
+        System.out.println(fixPriceProduct1.getStringRepresentation());
+
+        System.out.println("\nКоличество элементов в массиве: " + searchEngine.getSizeArr());
+
+        System.out.println("\nВывод полученного массива из поиска:");
+        Searchable[] searcha1 = searchEngine.search("Лимон");
+        System.out.println(Arrays.toString(searcha1));
+        Searchable[] searcha2 = searchEngine.search("Огурец");
+        System.out.println(Arrays.toString(searcha2));
+        Searchable[] searcha3 = searchEngine.search("Шоколад");
+        System.out.println(Arrays.toString(searcha3));
+        Searchable[] searcha4 = searchEngine.search("Заголовок 1");
+        System.out.println(Arrays.toString(searcha4));
+        Searchable[] searcha5 = searchEngine.search("Неизвестный текст");
+        System.out.println(Arrays.toString(searcha5));
+
+        SearchEngine testSearch = new SearchEngine(10);
+        testSearch.add(product1);
+        testSearch.add(product2);
+        testSearch.add(product3);
+        testSearch.add(product1);
+        testSearch.add(product2);
+        testSearch.add(product3);
+        testSearch.add(product1);  // Огурцов 6, выведет 5
+        testSearch.add(product1);
+        testSearch.add(product1);
+        testSearch.add(product1);
+
+        System.out.println("\nТест на количество в массиве searh:");
+        Searchable[] test1 = testSearch.search("Огурец");
+        System.out.println(Arrays.toString(test1));
+
     }
 }
