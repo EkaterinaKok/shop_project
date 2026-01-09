b104e6eb799b02ac473c4998ebacb4ac0585a515
User-PC <2356532@mail.ru>
2026-01-09 12:22:25 +0500

    ДЗ теме "Java Collections Framework: Set".

--- src/org/skypro/skyshop/articles/Article.java
+++ src/org/skypro/skyshop/articles/Article.java
@@ -1,5 +1,7 @@
 package org.skypro.skyshop.articles;
 
+import java.util.Objects;
+
 public class Article implements Searchable {
 
     private String title;
@@ -32,4 +34,18 @@ public class Article implements Searchable {
     public String getContentType() {
         return "ARTICLE";
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return Objects.equals(title, article.title);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(title);
+    }
+
 }
--- src/org/skypro/skyshop/articles/Searchable.java
+++ src/org/skypro/skyshop/articles/Searchable.java
@@ -3,6 +3,7 @@ package org.skypro.skyshop.articles;
 import org.skypro.skyshop.product.Product;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public interface Searchable {
 
@@ -13,5 +14,21 @@ public interface Searchable {
     default String getStringRepresentation() {
         return this.getSearchTerm() + "\n" + this.getContentType();
     }
+
+    public class SearchComporator implements Comparator<Searchable> {
+        @Override
+        public int compare(Searchable s1, Searchable s2) {
+            int lengthComparison = Integer.compare(
+                    s2.getSearchTerm().length(),
+                    s1.getSearchTerm().length());
+
+            if (lengthComparison != 0) {
+                return lengthComparison;
+            }
+            return s1.getSearchTerm().compareToIgnoreCase(s2.getSearchTerm());
+        }
+    }
+
+
 
 }
